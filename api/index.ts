6e91539c15168e674259c1df0ed772db66b07691
Ella N <38847123+ellanan@users.noreply.github.com>
2022-03-02 22:09:32 -0500

    add comments and logs

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
-// The VercelRequest and VercelResponse imports are types for the Request and Response objects
 import _ from 'lodash';
+// The VercelRequest and VercelResponse imports are types for the Request and Response objects
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { DateTime, Interval } from 'luxon';
 import { getDataByDate } from './_data/getDataByDate';
@@ -25,18 +25,21 @@ function getData(args: OrignalAPIQueryParams & AdditionalQueryParams): {
   cacheDurationMinutes: number;
   data: typeof dailyData | typeof dailyData[number];
 } {
+  // if no agruments are passed, return the current day data
   if (Object.keys(args).length === 0) {
     return {
       cacheDurationMinutes: 30,
       data: dailyData.slice(-1)[0],
     };
   }
+  // if a date is passed, return the data for that date
   if (args.date) {
     return {
       cacheDurationMinutes: 60,
       data: dataDictonary[args.date],
     };
   }
+  // if a start_date and end_date are passed, return the data for that date range
   if (args.start_date && args.end_date) {
     const dateInputRegex = /(\d{4})-(\d{1,})-(\d{1,})/;
     const [, startYear, startMonth, startDate] =
@@ -64,6 +67,7 @@ function getData(args: OrignalAPIQueryParams & AdditionalQueryParams): {
       ),
     };
   }
+  // if start_date is passed, return the data for that date and all future days
   if (args.start_date) {
     const dateInputRegex = /(\d{4})-(\d{1,})-(\d{1,})/;
     const [, startYear, startMonth, startDate] =
@@ -83,6 +87,7 @@ function getData(args: OrignalAPIQueryParams & AdditionalQueryParams): {
       data: dailyData.slice(startingIndex, startingIndex + limit),
     };
   }
+  // if count is passed, return a random subset of the data
   if (args.count) {
     return {
       cacheDurationMinutes: 0,
@@ -103,10 +108,12 @@ export default async (request: VercelRequest, response: VercelResponse) => {
     DateTime.now().startOf('day')
   ).length('days');
 
+  // handle the case where some data is missing
   if (daysSinceLastData >= 1) {
     console.log(
       `missing ${daysSinceLastData} days of data from ${lastDayOfData}`
     );
+    // try to get the missing data from the last day of data
     const missingData = await Promise.all(
       Interval.fromDateTimes(
         DateTime.fromISO(lastDayOfData).startOf('day').plus({ days: 1 }),
@@ -117,6 +124,7 @@ export default async (request: VercelRequest, response: VercelResponse) => {
         })
         .map(async (interval) => {
           try {
+            console.log(`getting data for ${interval.start.toISODate()}`);
             return getDataByDate(interval.start);
           } catch (error) {
             console.error(`could not fetch data for ${interval.start}`, error);
@@ -124,6 +132,7 @@ export default async (request: VercelRequest, response: VercelResponse) => {
           }
         })
     );
+
     const missingDataPairs = missingData.flatMap((entry) => {
       if (entry) {
         return [[entry.date, entry]] as [[string, typeof entry]];
@@ -131,6 +140,7 @@ export default async (request: VercelRequest, response: VercelResponse) => {
       return [];
     });
 
+    console.log(`adding ${missingDataPairs.length} missing data pairs`);
     if (missingDataPairs.length > 0) {
       Object.assign(dataDictonary, _.fromPairs(missingDataPairs));
       isoDates.push(...missingDataPairs.map(([date, data]) => data.date));
@@ -138,14 +148,16 @@ export default async (request: VercelRequest, response: VercelResponse) => {
       lastDayOfData = missingDataPairs[missingDataPairs.length - 1][0];
     }
   }
+
   const { cacheDurationMinutes, data } = getData(queryParams);
+
   response
     .status(200)
     .setHeader(
       'Cache-Control',
       `max-age=0, s-maxage=${
         cacheDurationMinutes * 60
-      }, stale-while-revalidate=${cacheDurationMinutes * 60}`
+      }, stale-while-revalidate=${cacheDurationMinutes * 60}` // cache could reuse a stale response while revalidating
     )
     .send(data);
 };
